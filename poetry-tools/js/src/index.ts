19691c4a028102899b91465c382469ef83595cfb
Ken Fox <fox@xythian.com>
2025-03-26 18:01:04 -0700

    Fix TypeScript build issues and use Bun for building

--- poetry-tools/js/src/index.ts
+++ poetry-tools/js/src/index.ts
@@ -3,4 +3,4 @@ export { WordlistGenerator } from './wordlist-generator';
 export { IPv6PoetryConverter } from './converters';
 
 // Re-export the CLI entrypoint for bin script
-export { default as cli } from './cli';
+export * as cli from './cli';
